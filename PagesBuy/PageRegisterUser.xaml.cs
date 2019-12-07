f31a1c4c91414d8c9f0ed361337f07fe8a4a8425
Sudkir <sud-kir@yandex.ru>
2019-12-07 14:15:29 +0300

    Добавлены основные окна

---
+++ PagesBuy/PageRegisterUser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SystemSellBuy.PagesBuy
+{
+    /// <summary>
+    /// Логика взаимодействия для PageRegisterUser.xaml
+    /// </summary>
+    public partial class PageRegisterUser : Page
+    {
+        public PageRegisterUser()
+        {
+            InitializeComponent();
+            datePickerNew.DisplayDate = DateTime.Now;
+            datePickerNew.Text = DateTime.Now.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("","",MessageBoxButton.OK);
+            
+            WindowForPage.OpenLoginPage();
+        }
+
+        private void ToLogin(object sender, RoutedEventArgs e)
+        {
+            WindowForPage.OpenLoginPage();
+        }
+    }
+}
---
+++ PagesBuy/WindowForPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemSellBuy.PagesBuy
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowForPage.xaml
+    /// </summary>
+    public partial class WindowForPage : Window
+    {
+
+       private static Frame frame;
+
+        public WindowForPage()
+        {
+            InitializeComponent();
+            frame = MainFrame;
+            frame.Navigate(new PageLogin());
+        }
+
+        public static void RegPage() {
+           
+            frame.Navigate(new PageRegisterUser());
+        }
+
+        public static void OpenMenu()
+        {
+
+            frame.Navigate(new PageMenu());
+        }
+
+        public static void OpenInfo()
+        {
+
+            frame.Navigate(new PageInfoUser());
+        }
+
+        public static void OpenNewBuy()
+        {
+
+            frame.Navigate(new PageNewBuy());
+        }
+
+        public static void OpenData()
+        {
+
+            frame.Navigate(new PageData());
+        }
+
+        public static void OpenLoginPage()
+        {
+
+            frame.Navigate(new PageLogin());
+        }
+    }
+}
